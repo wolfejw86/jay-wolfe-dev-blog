4a3c197243d372cf3fcb6e6d92b683259d59ea09
John Wolfe <wolfejw86@gmail.com>
2019-09-02 23:06:09 -0400

    init

---
+++ content/blog/intro-to-change-streams/index.md
@@ -0,0 +1,10 @@
+---
+title: "Intro To MongoDB Change Streams"
+tags: ["mongodb"]
+published: true
+date: "2019-03-20"
+---
+
+Curabitur tincidunt hendrerit justo. Praesent nisl dui, mollis vitae quam eu, dignissim faucibus lorem. Sed vitae erat at sem suscipit laoreet nec vel diam.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/-BIQjRd3NGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
---
+++ gatsby-config.js
@@ -0,0 +1,81 @@
+"use strict"
+
+const siteConfig = require("./config")
+
+module.exports = {
+  siteMetadata: {
+    url: siteConfig.url,
+    title: siteConfig.title,
+    tagline: siteConfig.tagline,
+    description: `A blog template for web developers that's ready to go out of the box. Feel free to modify it to your liking.`,
+    author: siteConfig.author.name,
+    contacts: {
+      linkedin: siteConfig.author.contacts.linkedin,
+      github: siteConfig.author.contacts.github,
+      stackoverflow: siteConfig.author.contacts.stackoverflow,
+      twitter: siteConfig.author.contacts.twitter,
+    },
+    labels: siteConfig.labels,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+          `gatsby-remark-responsive-iframe`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Raleway`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
